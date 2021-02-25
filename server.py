fb4fb93f97fd4cf553a5049566db76d908e92ed4
Thies <thies.h13@gmail.com>
2021-02-25 18:02:43 +0100

    Added hearbeat for fire detectors and broadcast of server ip

--- server.py
+++ server.py
@@ -4,10 +4,17 @@ from sim800l import SIM800L
 from gmail import Gmail
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.redis import RedisJobStore
+from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, gethostbyname, gethostname
+from datetime import datetime
 
 app = Flask(__name__)
 redis = Redis(host='localhost', port=6379)
 sim = SIM800L("/dev/serial0")
+
+broadcastSocket = socket(AF_INET, SOCK_DGRAM)
+broadcastSocket.bind(('', 0))
+broadcastSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+my_ip = gethostbyname(gethostname())
 
 
 @app.route('/fire/alarm')
@@ -20,6 +27,13 @@ def fire_alarm():
 @app.route('/fire/register/<id>')
 def register_fire_detector(id):
     redis.sadd('fire:detectors', id)
+    fire_detector_heartbeat(id)
+
+
+@app.route('/fire/heartbeat/')
+def fire_detector_heartbeat(id):
+    redis.set('fire:heartbeats:{}'.format(id), str(datetime.now()))
+    redis.expire('fire:heartbeats:{}'.format(id), 360)
 
 
 def check_heartbeats():
@@ -31,6 +45,13 @@ def check_heartbeats():
                             message_text="Von {} wurde kein Heartbeat mehr empfangen, schau besser mal nach was da los ist.".format(detector))
 
 
+def broadcast():
+    data = 'SMS-Server:' + gethostbyname(gethostname())
+    broadcastSocket.sendto(data, ('<broadcast>', 44566))
+
+
 scheduler = BackgroundScheduler(jobstores={'redis': RedisJobStore(db=1)})
 scheduler.add_job(check_heartbeats, 'interval',
                   minutes=5, id='check_heartbeats')
+scheduler.add_job(broadcast, 'interval',
+                  minutes=5, id='broadcast')
